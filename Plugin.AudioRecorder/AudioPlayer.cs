d03f43fa27dee98264202c08959b29cdb36d1077
wang xuan <wx_0322@163.com>
2023-01-16 10:02:00 +0800

    直接使用源码进行功能测试完成

--- Plugin.AudioRecorder/AudioPlayer.cs
+++ Plugin.AudioRecorder/AudioPlayer.cs
@@ -1,13 +1,58 @@
-﻿using System;
+using Android.Media;
+using System;
 
 namespace Plugin.AudioRecorder
 {
 	public partial class AudioPlayer
-    {
-		public void Play (string pathToAudioFile) => throw new NotImplementedException ();
+	{
+		private MediaPlayer mediaPlayer;
 
-		public void Pause () => throw new NotImplementedException ();
+		public AudioPlayer()
+		{
+		}
 
-		public void Play () => throw new NotImplementedException ();
+		public void Play(string pathToAudioFile)
+		{
+			if (mediaPlayer != null)
+			{
+				mediaPlayer.Completion -= MediaPlayer_Completion;
+				mediaPlayer.Stop();
+			}
+
+			if (pathToAudioFile != null)
+			{
+				if (mediaPlayer == null)
+				{
+					mediaPlayer = new MediaPlayer();
+
+					mediaPlayer.Prepared += (sender, args) =>
+					{
+						mediaPlayer.Start();
+						mediaPlayer.Completion += MediaPlayer_Completion;
+					};
+				}
+
+				mediaPlayer.Reset();
+				//_mediaPlayer.SetVolume (1.0f, 1.0f);
+
+				mediaPlayer.SetDataSource(pathToAudioFile);
+				mediaPlayer.PrepareAsync();
+			}
+		}
+
+		void MediaPlayer_Completion(object sender, EventArgs e)
+		{
+			FinishedPlaying?.Invoke(this, EventArgs.Empty);
+		}
+
+		public void Pause()
+		{
+			mediaPlayer?.Pause();
+		}
+
+		public void Play()
+		{
+			mediaPlayer?.Start();
+		}
 	}
 }
